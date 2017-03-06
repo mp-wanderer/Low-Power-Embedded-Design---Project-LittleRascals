f232b18e98dbdccffb88103964a2d562553d7ef3
Rishabh Berlia <rishabh.berlia@colorado.edu>
2017-03-06 01:02:40 -0700

    Create ble_functions.h

---
+++ Firmware/ble_functions.h
@@ -0,0 +1,18 @@
+/*
+ * ble_functions.h
+ *
+ * Created: 12/11/2016 12:07:08 PM
+ *  Author: Shiva and Rishabh
+ */ 
+
+
+#ifndef BLE_FUNCTIONS_H_
+#define BLE_FUNCTIONS_H_
+
+#include <asf.h>
+#include "main.h"
+#include "ble_manager.h"
+#include "ble_utils.h"
+#include "at_ble_api.h"
+
+#endif /* BLE_FUNCTIONS_H_ */
